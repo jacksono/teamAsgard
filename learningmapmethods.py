af68977b459150a9406cdfd86c7650c69238f28f
sharonsyra <sharonwaithii@gmail.com>
2017-02-10 15:02:11 +0300

    Learning Map methods

--- learningmapmethods.py
+++ learningmapmethods.py
@@ -7,10 +7,12 @@ class LearningMap():
         for skill, value in dict_skills.items():
             if value == 1:
                 skills_list.append(skill)
+        print("You have completed the skills below:")
         return skills_list
     def view_unstudied_skills(self, skill):
         skills_list = []
         for skill, value in dict_skills.items():
             if value == 0:
                 skills_list.append(skill)
+        print("You are yet to cover these skills:")
         return skills_list
